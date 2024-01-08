c4644c002d2bf508c0c88e194cb4fe67789b30ad
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-01-08 10:31:43 -0700

    made error catch dynamic

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -7,6 +7,7 @@ const URL = 'http://localhost:9000/api/todos'
 export default class App extends React.Component {
   state = {
     todos: [],
+    error: '',
   }
   
   fetchAllTodos = () =>{
@@ -15,7 +16,7 @@ export default class App extends React.Component {
       this.setState({...this.state, todos: res.data.data})
     })
     .catch(err => {
-     
+     this.setState({...this.state, error: err.response.data.message})
     })
   }
 
@@ -28,7 +29,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div id='error'>Error: No error here</div>
+        <div id='error'>{this.state.error}</div>
         <div id='todos'>
           <h2>Todos:</h2>
           {
