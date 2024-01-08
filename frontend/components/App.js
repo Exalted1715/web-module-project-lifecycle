4a6ce8ff04d773007bb0f183a7b58e1a1d922a9a
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-01-08 12:40:08 -0700

    form broken out

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import axios  from 'axios'
+import Form from './Form'
 
 
 const URL = 'http://localhost:9000/api/todos'
@@ -87,12 +88,14 @@ export default class App extends React.Component {
            // })
           } 
         </div>
-      <form id="todoForm" onSubmit={this.onTodoFormSubmit}>
-        <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type="text" placeholder='Type todo'></input>
-        <input type='submit'></input>
-      </form>
-      <button onClick={this.toggleDisplayCompleted}>{this.state.displayCompleted ? 'Hide' : "Show"} Completed</button>
-    </div>
+        <Form
+        onTodoFormSubmit = {this.onTodoFormSubmit}
+        todoNameInput = {this.state.todoNameInput}
+        onTodoNameInputChange = {this.onTodoNameInputChange}
+        toggleDisplayCompleted = {this.toggleDisplayCompleted}
+        displayCompleted = {this.state.displayCompleted}
+        />
+   </div>
     )
   }
 }
